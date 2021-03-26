2750db0f8cf8f52ca75a3e7bbd157b6bc3a2693d
Walter TimothyJos Lopez <Walter.Lopez@everbank.com>
2021-03-25 22:27:31 -0400

    Initial Commit

---
+++ HealthCheck/ICMPHealthCheck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace HealthCheck
+{
+    public class ICMPHealthCheck : IHealthCheck
+    {
+        private readonly string Host;
+        private readonly int HealthyRoundtripTime;
+
+        public ICMPHealthCheck(string host, int healthyRoundtripTime)
+        {
+            Host = host;
+            HealthyRoundtripTime = healthyRoundtripTime;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                using var ping = new Ping();
+                var reply = await ping.SendPingAsync(Host);
+
+                switch (reply.Status)
+                {
+                    case IPStatus.Success:
+                        var msg = $"ICMP to {Host} took {reply.RoundtripTime} ms.";
+
+                        return (reply.RoundtripTime > HealthyRoundtripTime)
+                            ? HealthCheckResult.Degraded(msg)
+                            : HealthCheckResult.Healthy(msg);
+
+                    default:
+                        var err = $"ICMP to {Host} failed: {reply.Status}";
+                        return HealthCheckResult.Unhealthy(err);
+                }
+            }
+            catch (Exception ex)
+            {
+                var err = $"ICMP to {Host} failed: {ex.Message}";
+                return HealthCheckResult.Unhealthy(err);
+            }
+        }
+    }
+}
